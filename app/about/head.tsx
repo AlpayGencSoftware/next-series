dc4d131d2f3aeb0f030dcbe7bc1b470edd8c0fe2
AlpayGencSoftware <gencoalpay@gmail.com>
2023-08-09 16:32:12 +0300

    All data prepared and error page added

---
+++ app/about/head.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Head() {
+  return (
+   <>
+     <title>About Page</title>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+     <meta name="description" content="Created by Alpay"/>
+     <link rel="icon" href="/favicon.ico"/>
+   </>
+  )
+}
